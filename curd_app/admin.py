73f5e1b1f3214284d3aa1ad0c06080b7ce6da768
Harsh <harshuttekarweb999@gmail.com>
2026-08-11 12:24:31 +0530

    Remove Django backend

--- curd_app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from curd_app.models import Employ_Att, Employ_Data 
-
-# Register your models here.
-admin.site.register(Employ_Data)
-admin.site.register(Employ_Att)
